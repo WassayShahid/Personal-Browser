1a5eaf17957ec776b8c5b3abad27036ef51037a3
Muhammad Wassay Shahid <wassay.shahid@gmail.com>
2026-08-04 23:58:24 -0400

    2nd Commit
     Functional Browser

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 from PyQt6.QtWidgets import *
+from PyQt6.QtGui import QAction
 from PyQt6.QtWebEngineWidgets import *
 from PyQt6.QtCore import *
 
@@ -12,8 +13,47 @@ class MainWindow(QMainWindow):
         self.setWindowTitle("Gnoiss")
         self.showMaximized()
 
+        # Navbar
+        self.navbar = QToolBar()
+        self.addToolBar(self.navbar)
 
+        # URL bar
+        self.url_bar = QLineEdit()
+        self.url_bar.returnPressed.connect(self.navigate_to_url)
+        self.navbar.addWidget(self.url_bar)
+        self.browser.urlChanged.connect(self.update_url)
 
+        # Back button
+        self.back_button = QAction("Back", self)
+        self.back_button.triggered.connect(self.browser.back)
+        self.navbar.addAction(self.back_button)
+
+        # Forward button
+        self.forward_button = QAction("Forward", self)
+        self.forward_button.triggered.connect(self.browser.forward)
+        self.navbar.addAction(self.forward_button)
+
+        # Reload button
+        self.reload_button = QAction("Reload", self)
+        self.reload_button.triggered.connect(self.browser.reload)
+        self.navbar.addAction(self.reload_button)
+
+        # Home button
+        self.home_button = QAction("Home", self)
+        self.home_button.triggered.connect(self.navigate_home)
+        self.navbar.addAction(self.home_button)
+
+    def navigate_home(self):
+        self.browser.setUrl(QUrl("https://www.google.com"))
+
+    def navigate_to_url(self):
+        url = self.url_bar.text()
+        if not url.startswith("http"):
+            url = "http://" + url
+        self.browser.setUrl(QUrl(url))
+
+    def update_url(self, q):
+        self.url_bar.setText(q.toString())
 
 app = QApplication(sys.argv)
 window = MainWindow()
